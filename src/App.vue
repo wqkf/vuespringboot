066b181f6fdbe43f63be45012428f42d2a0abf41
sss <1511218972@qq.com>
2021-09-24 11:49:29 +0800

    sch3

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
   <div id="app">
    <van-nav-bar
-  title="标题"
+  title=""
   left-text="返回"
   left-arrow
   @click-left="onClickLeft"
@@ -11,8 +11,8 @@
     <router-view/>
     <van-tabbar v-model="active" active-color="#ee0a24" inactive-color="#000">
   <van-tabbar-item icon="home-o" to="/">主页</van-tabbar-item>
-  <van-tabbar-item icon="search" to="/search">检索</van-tabbar-item>
-  <van-tabbar-item icon="friends-o" to="/userInfo">好友</van-tabbar-item>
+  <van-tabbar-item icon="coupon-o"  to="/search">书架</van-tabbar-item>
+  <van-tabbar-item icon="fire-o"  to="/userInfo">圈子</van-tabbar-item>
   <van-tabbar-item icon="shop-collect" to="/order">购物车</van-tabbar-item>
   <van-tabbar-item icon="setting-o" to="/order">设置</van-tabbar-item>
 </van-tabbar>
@@ -31,7 +31,7 @@ export default {
   methods: {
     onClickLeft() {
      this.$router.go(-1)
-      Toast('返回');
+      
     },
     
   },
